b17370a93ca14fcc877f4ba80b6ec26824f79261
JonathanRivera05 <jonathanvasquez842@gmail.com>
2026-05-04 01:20:51 -0600

    Update General
    index.html
    main.css
    portfolio.js
    CV

--- Elora/HTML/assets/js/portfolio.js
+++ Elora/HTML/assets/js/portfolio.js
@@ -14,6 +14,51 @@ $(document).ready(function(){
         }
     })
 
+
+    // MAGNIFIC-POPUP CORREGIDO
+    // En lugar de aplicar el popup a toda la ".gallery", 
+    // lo aplicamos a cada ".portfolio-item" por separado.
+    $(".portfolio-item").each(function() {
+        $(this).magnificPopup({
+            delegate: "a",
+            type: "image",
+            removalDelay: 500, //delay removal by X to allow out-animation
+            gallery: {
+                enabled: true
+            },
+
+            callbacks: {
+                beforeOpen: function() {
+                  // just a hack that adds mfp-anim class to markup 
+                    this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                    // Extrae el efecto visual del atributo data-effect del enlace clicado
+                    this.st.mainClass = this.st.el.attr('data-effect');
+                }
+            },
+            closeOnContentClick: true,
+            midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+        })
+    })
+});
+
+/** CODIGO ANT
+$(document).ready(function(){
+
+    $(".button").click(function(){
+        $(this).addClass("active").siblings().removeClass("active");
+
+        var filter = $(this).attr("data-filter");
+    
+        if (filter == "all"){
+            $(".gallery .image").show(400);
+        }
+        else{
+            $(".gallery .image").not("."+filter).hide(200);
+            $(".gallery .image").filter("."+filter).show(400);
+        }
+    })
+
+
     //MAGNIFIC-POPUP
     $(".gallery").magnificPopup({
         
@@ -38,3 +83,5 @@ $(document).ready(function(){
 
 
 });
+
+ */
